d320ea41753be5a06765e9e69d20d1e31e8da0ff
Benjamin Stickel <fin09pcap@gmail.com>
2016-02-04 13:43:33 -0800

    updated error for `_display_error`

--- freight_forwarder/utils/utils.py
+++ freight_forwarder/utils/utils.py
@@ -349,8 +349,8 @@ def _display_error(normalized_data, stream):
     # TODO: need to revisit this later.
     error = normalized_data['error']
     if 'error_detail' in normalized_data:
-        if 'code' in normalized_data:
-            stream.write("exit code: {0}".format(normalized_data['code']))
+        if normalized_data['error_detail'].get('code'):
+            stream.write("exit code: {0}\n".format(normalized_data['error_detail'].get('code')))
 
         if 'error_detail' in normalized_data:
             stream.write(normalized_data['error_detail'].get('message', 'There were no message details provided.'))
