c978852d9210bc0cbb6d6bfa7420229d40e43e5d
Benjamin Stickel <fin09pcap@gmail.com>
2016-02-08 19:35:07 -0800

    fixed lint errors

--- freight_forwarder/utils/utils.py
+++ freight_forwarder/utils/utils.py
@@ -356,14 +356,10 @@ def _display_error(normalized_data, stream):
     # TODO: need to revisit this later.
     error = normalized_data['error']
     if 'error_detail' in normalized_data:
-        stream.write("exit code: {0}\n".format(
-                normalized_data['error_detail'].get('code'),
-                'There was no exit code provided'
-            )
-        )
+        stream.write("exit code: {0}\n".format(normalized_data['error_detail'].get('code'),
+                                               'There was no exit code provided'))
 
-        stream.write(
-                normalized_data['error_detail'].get('message', 'There were no message details provided.'))
+        stream.write(normalized_data['error_detail'].get('message', 'There were no message details provided.'))
 
     raise DockerStreamException(error)
 
